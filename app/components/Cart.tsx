f76cfbd2524da3a7b21e4a4bf0cc3488268eca61
NpF <natdanai.prs@gmail.com>
2024-05-13 17:15:50 +0700

    creating Cart component to display selected meals

---
+++ app/components/Cart.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { FloatButton, Modal } from 'antd'
+import { Meal } from '@/lib/interface'
+
+
+function Cart({mealOrders}: {mealOrders: Meal[]}) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <FloatButton onClick={showModal} badge={{ count: mealOrders.length }} icon={<ShoppingCartIcon />} />
+
+      <Modal title="My Cart" open={isModalOpen} onOk={handleOk} okText="Confirm" centered={true} onCancel={handleCancel}>
+        {
+          mealOrders?.map((meal, key) => (
+            <p key={key}>{meal.strMeal}</p>
+          ))
+        }
+      </Modal>
+    </>
+  )
+}
+
+export default Cart
