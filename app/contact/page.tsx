29b33a809f1c09fa68655966404778265899d769
NpF <natdanai.prs@gmail.com>
2024-05-13 17:36:55 +0700

    contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Skeleton } from 'antd';
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <Skeleton />
+    </div>
+  )
+}
